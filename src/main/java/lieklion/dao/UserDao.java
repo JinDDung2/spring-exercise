22f3302df143324825dc0704730df61b841a9237
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-25 12:57:53 +0900

    Exception처리

--- src/main/java/lieklion/dao/UserDao.java
+++ src/main/java/lieklion/dao/UserDao.java
@@ -15,44 +15,91 @@ public class UserDao {
     }
 
     public void save(User user) {
+        String sql = "INSERT INTO users(id, name, password) VALUES(?,?,?)";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+
         try {
-            Connection conn = connectionMaker.makeConnection();
-            PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement(sql);
             ps.setString(1, user.getId());
             ps.setString(2, user.getName());
             ps.setString(3, user.getPassword());
 
             ps.executeUpdate();
-            close(conn, ps);
-
         } catch (SQLException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
+        } finally {
+            close(conn, ps);
         }
     }
 
     public User findById(String id) {
+        String sql = "SELECT * FROM users WHERE users.id=?";
 
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        String sql = "SELECT * FROM users WHERE users.id=?";
-
         try {
             conn = connectionMaker.makeConnection();
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+            PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, id);
             rs = pstmt.executeQuery();
 
-            rs.next();
-            User user = new User(rs.getString("id"), rs.getString("name"),
-                    rs.getString("password"));
-
-            close(conn, ps, rs);
-            return user;
+            if (rs.next()) {
+                User user = new User(rs.getString("id"), rs.getString("name"),
+                        rs.getString("password"));
+                return user;
+            } else {
+                throw new NullPointerException();
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            close(conn, ps, rs);
+        }
+    }
+
+    public void deleteAll() {
+        String sql = "DELETE FROM users";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        finally {
+            close(conn, ps);
+        }
+    }
+
+    public int getCount() {
+        String sql = "SELECT COUNT(*) FROM users";
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = connectionMaker.makeConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, ps);
         }
     }
 
@@ -60,10 +107,12 @@ public class UserDao {
     // 가변인자로 받아서, 파라미터를 여러개 받는다.
     private void close(AutoCloseable... autoCloseable) {
         for (AutoCloseable ac : autoCloseable) {
-            try {
-                ac.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            if (ac != null) {
+                try {
+                    ac.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
--- src/test/java/lieklion/dao/UserDaoTest.java
+++ src/test/java/lieklion/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import lieklion.connection.AwsConnection;
 import lieklion.connection.ConnectionMaker;
 import lieklion.domain.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,15 +27,44 @@ class UserDaoTest {
     @BeforeEach
     void setUp() {
         this.userDao = context.getBean("awsUserDao", UserDao.class);
-        userDao.save(new User("0", "default", "0000"));/**/
+        userDao.deleteAll();
+        userDao.save(new User("0", "default", "0000"));
     }
 
     @Test
+    @DisplayName("저장과 찾기")
     void saveAndFindById() {
-        String id = "0";
-        userDao.save(new User(id, "zero", "1234"));
+        String id = "11";
+        userDao.save(new User(id, "IVE", "0000"));
         User findUser = userDao.findById(id);
-        assertEquals("zero", findUser.getName());
+        assertEquals("IVE", findUser.getName());
+    }
+
+    @Test
+    @DisplayName("save할 게 없으면 예외처리")
+    void addException() {
+        userDao.deleteAll();
+        User user = null;
+        assertThrows(NullPointerException.class, () -> {
+            userDao.save(user);
+        });
+    }
+
+    @Test
+    @DisplayName("find할 수 없으면 예외처리")
+    void findByIdEmpty() {
+        assertThrows(RuntimeException.class, () -> {
+            userDao.findById("1000");
+        });
+
+    }
+
+    @Test
+    @DisplayName("갯수 세기")
+    void getCount() {
+        assertEquals(1, userDao.getCount());
+        userDao.save(new User("300", "homidle", "1234"));
+        assertEquals(2, userDao.getCount());
     }
 
 
